1746337079fd70dc2215671d2a68d5e746871eca
Stew <github@stew.dk>
2011-11-01 03:43:28 -0400

    New algorithm for the accelerometer

--- src/testaccelerometer.c
+++ src/testaccelerometer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
@@ -6,7 +7,7 @@
 #include "sio.h"
 
 //how many ADC samples to sum together
-#define AVG_COUNT 24U
+#define AVG_COUNT 24
 
 //global variable to let the main program know that an A/D conversion finished
 static volatile int8_t gADC = 0;
@@ -35,31 +36,30 @@ static void set_DDR(void) {
 	DDRC |= _BV(PC6);
 	PORTC |= _BV(PC6);
 
-	// Enable PB7-PB2 as output for LEDs
-	DDRB |= _BV(PB7) | _BV(PB6) | _BV(PB5) | _BV(PB4) | _BV(PB3) | _BV(PB2);
-	PORTB |= _BV(PB7) | _BV(PB5) | _BV(PB3);
-	PORTB &= ~_BV(PB6) & ~_BV(PB4) & ~_BV(PB2);
+	// Enable PB7-PB3 as output for LEDs
+	DDRB |= _BV(PB7) | _BV(PB6) | _BV(PB5) | _BV(PB4) | _BV(PB3);
+	PORTB &= ~_BV(PB7) & ~_BV(PB6) & ~_BV(PB5) & ~_BV(PB4) & ~_BV(PB3);
 }
 
 void setLEDs(uint16_t accelValue)
 {
-	if (accelValue < 11000UL)
+	if (accelValue < 1375UL)
 	{
-		PORTB &= ~_BV(PB7) & ~_BV(PB5) & ~_BV(PB3);
+		PORTB &= ~_BV(PB7) & ~_BV(PB6) & ~_BV(PB5) & ~_BV(PB4) & ~_BV(PB3);
 	}
-	else if (accelValue < 11500UL)
+	else if (accelValue < 1675UL)
 	{
-		PORTB |= _BV(PB7);
-		PORTB &= ~_BV(PB5) & ~_BV(PB3);
+		PORTB |= _BV(PB4);
+		PORTB &= ~_BV(PB7) & ~_BV(PB6) & ~_BV(PB5) & ~_BV(PB3);
 	}
-	else if (accelValue < 12000UL)
+	else if (accelValue < 1975UL)
 	{
-		PORTB |= _BV(PB7) | _BV(PB5);
-		PORTB &= ~_BV(PB3);
+		PORTB |= _BV(PB3) | _BV(PB5) | _BV(PB4);
+		PORTB &= ~_BV(PB6) & ~_BV(PB7);
 	}
 	else
 	{
-		PORTB |= _BV(PB7) | _BV(PB5) | _BV(PB3);
+		PORTB |= _BV(PB7) | _BV(PB6) | _BV(PB5) | _BV(PB4) | _BV(PB3);
 	}
 }
 
@@ -124,9 +124,48 @@ ISR(ADC_vect) {
 	TIFR0 = _BV(OCF0A);
 }
 
+//integer square root routine (http://www.finesse.demon.co.uk/steven/sqrt.html)
+#define iter1(N) \
+    try = root + (1 << (N)); \
+    if (n >= try << (N))   \
+    {   n -= try << (N);   \
+        root |= 2 << (N); \
+    }
+
+uint32_t isqrt(uint32_t n)
+{
+    uint32_t root = 0, try;
+    iter1 (15);    iter1 (14);    iter1 (13);    iter1 (12);
+    iter1 (11);    iter1 (10);    iter1 ( 9);    iter1 ( 8);
+    iter1 ( 7);    iter1 ( 6);    iter1 ( 5);    iter1 ( 4);
+    iter1 ( 3);    iter1 ( 2);    iter1 ( 1);    iter1 ( 0);
+    return root >> 1;
+}
+
 int main(void) {
-	uint16_t xsum = 0, ysum = 0, zsum = 0;
+	uint16_t xSum = 0, ySum = 0, zSum = 0;
 	uint8_t position = 0;
+
+	/* experimental results with with AVG_COUNT = 24
+	x: 10000 - 12790
+	range: 2790
+	mid: 11395
+
+	y: 10610 - 13450
+	range: 2840
+	mid: 12030
+
+	z: 11080 - 13780
+	range: 2700
+	mid: 12430
+	*/
+
+	const uint16_t xZero = 475 * AVG_COUNT; //500;
+	const uint16_t yZero = 501 * AVG_COUNT; //500;
+	const uint16_t zZero = 518 * AVG_COUNT; //535;
+
+	int16_t x, y, z;
+	uint32_t magnitude;
 
 	MCUCR |= _BV(JTD);		// Turn off JTAG interface to allow control of upper Port C pins
 	MCUCR |= _BV(JTD);		// Must be done twice within 4 cycles to take effect
@@ -155,22 +194,37 @@ int main(void) {
 				sei();
 
 				if (gLastADCChannel == 0) { //X
-					xsum += gXADCValue;
+					xSum += gXADCValue;
 				} else if (gLastADCChannel == 1) { //Y
-					ysum += gYADCValue;
+					ySum += gYADCValue;
 				} else if (gLastADCChannel == 2) { //Z
-					zsum += gZADCValue;
+					zSum += gZADCValue;
 
 					position = (position + 1) % AVG_COUNT;
 
 
 					if (position == 0) {
+						x = xSum - xZero;
+						y = ySum - yZero;
+						z = zSum - zZero;
+						
+						//absolute value
+						x = x < 0 ? -x : x;
+						y = y < 0 ? -y : y;
+						z = z < 0 ? -z : z;
+
+						//Pythagorean theorem
+						magnitude = isqrt(
+							(uint32_t)x * (uint32_t)x +
+							(uint32_t)y * (uint32_t)y +
+							(uint32_t)z * (uint32_t)z);
+
 						cli();
 						//Apparently, printf doesn't like to be interrupted. Just try it.
-						//printf("%u,%u,%u,%u          \r", xsum, ysum, zsum, TCNT1);
-						setLEDs(ysum);
+						//printf("%lu,   %d,%d,%d,    %u          \r", magnitude, x, y, z, TCNT1);
+						setLEDs(magnitude);
 						sei();
-						xsum = ysum = zsum = 0;
+						xSum = ySum = zSum = 0;
 					}
 				} else {
 					break;
